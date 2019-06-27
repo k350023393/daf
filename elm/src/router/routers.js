f0c168059228e179865f666224a1a47540904e6e
k350023393 <243850872@qq.com>
2019-06-27 18:28:15 +0800

    删除无用文件

--- elm/src/router/routers.js
+++ elm/src/router/routers.js
@@ -1,10 +1,7 @@
 import App from '../App'
 import home from '../page/home/home'
-import login from '../page/login/login'
-import city from '../page/city/city'
 import redeem from '../page/daf/redeem'
 import record from '../page/daf/record'
-import test from '../page/daf/test'
 import assets from '../page/daf/assets'
 import assetsdetail from '../page/daf/assetsdetail'
 
@@ -19,20 +16,11 @@ export default [{
             path:'/home',
             component:home
         },{
-            path:'/login',
-            component:login
-        },{
-            path:'/city',
-            component:city
-        },{
             path:'/redeem',
             component:redeem
         },{
             path:'/record',
             component:record
-        },{
-            path:'/test',
-            component:test
         },{
             path:'/assets',
             component:assets
